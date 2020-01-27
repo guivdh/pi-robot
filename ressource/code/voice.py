d8174b49f928a76f3e39636df33ca00ae781dbed
Vanden Herrewegen Guillaume <guillaume-vdh@skynet.be>
2020-01-27 20:00:25 +0100

    update

--- ressource/code/voice.py
+++ ressource/code/voice.py
@@ -3,7 +3,7 @@ import time
 import playsound
 import speech_recognition as sr
 from gtts import gTTS
-#from omxplayer.player import OMXPlayer
+from omxplayer.player import OMXPlayer
 from pathlib import Path
 from time import sleep
 from mutagen.mp3 import MP3
@@ -12,28 +12,26 @@ def speak(mot):
     tts = gTTS(text=mot, lang='fr')
     filename = "voice.mp3"
     tts.save(filename)
-    playsound.playsound(filename)
-    #sound_path = Path(filename)
-    audio = MP3(filename)
-    #player = OMXPlayer(sound_path)
-    sleep(audio.info.length)
 
-    #player2=OMXPlayer(sound_path)
-    #sleep(audio.info.length)
-    #player.quit()
-    #player2.quit()
+    audio = MP3(filename)
+    player = OMXPlayer(filename, args=['-o', 'local'])
+    sleep(audio.info.length)
+    player.quit()
+
     os.remove('voice.mp3')
 
 def get_audio():
     r = sr.Recognizer()
     print('En écoute !')
-    playsound.playsound("Windows Pop-up Blocked.wav")
-    with sr.Microphone() as source:
-        audio = r.listen(source)
+    OMXPlayer("Windows Pop-up Blocked.wav", args=['-o', 'local'])
+    mic = sr.Microphone()
+    with mic as source:
+        audioSon = r.listen(source)
+        OMXPlayer("Windows Balloon.wav", args=['-o', 'local'])
         said = ""
 
         try:
-            said = r.recognize_google(audio, language='fr-FR')
+            said = r.recognize_google(audioSon, language='fr-FR')
             print(said)
         except Exception as e:
             print("Exception : " + str(e))
@@ -41,14 +39,16 @@ def get_audio():
     return said
 
 
-speak("Bonjour")
+speak("Bonjour, que puis-je faire pour vous?")
+print("Bonjour, que puis-je faire pour vous?")
+
 text = ""
 while("répète" not in text) :
     text = get_audio()
 
-while("stop" not in text) : 
+while("arrête" not in text) :
     text = get_audio()
-    if("stop" in text) :
-        speak("Ok, j'arrête de répéter!")
+    if("arrête" in text) :
+        print("Ok, j'arrête de répéter!")
     else :
         speak(text)
