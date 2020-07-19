6ebc2764fcd248e28cf49b59739d746f85fc8e01
guivdh <guillaume-vdh@skynet.be>
2020-07-19 18:18:16 +0200

    update site

--- ressource/site/login.js
+++ ressource/site/login.js
@@ -6,6 +6,13 @@ var path = require('path');
 const fs = require('file-system');
 const bcrypt = require('bcrypt');
 const http = require('http');
+const https = require('https');
+
+var privateKey = fs.readFileSync('key.pem');
+var certificate = fs.readFileSync('cert.pem');
+
+var credentials = {key: privateKey, cert: certificate};
+
 
 var connection = mysql.createConnection({
 	host     : '51.77.201.156',
@@ -14,7 +21,11 @@ var connection = mysql.createConnection({
 	database : 'nodelogin'
 });
 
+var credentials = {key: privateKey, cert: certificate};
+var express = require('express');
 var app = express();
+
+
 
 app.use(session({
 	secret: 'secret',
@@ -25,8 +36,10 @@ app.use(session({
 app.use(bodyParser.urlencoded({extended : true}));
 app.use(bodyParser.json());
 
+app.use(express.static('public'));
+
 app.get('/', function(request, response) {
-	response.sendFile(path.join(__dirname + '/login.html'));
+	response.sendFile(path.join(__dirname + '/index.html'));
 });
 
 app.post('/auth', function(request, response) {
@@ -59,7 +72,7 @@ app.get('/nbrBots', function (request, response) {
 			response.end(JSON.stringify(result));
 		});
 	} else {
-		response.send('Please login to view this page!');
+		response.send('Veuillez vous <a href="/login">connecter</a> pour accèder à ce contenu');
 	}
 })
 
@@ -71,8 +84,44 @@ app.get('/requests', function (request, response) {
 			response.end(JSON.stringify(result));
 		});
 	} else {
-		response.send('Please login to view this page!');
+		response.send('Veuillez vous <a href="/login">connecter</a> pour accèder à ce contenu');
 	}
+})
+
+app.get('/requestsToday', function (request, response) {
+	if (request.session.loggedin) {
+		var now = new Date();
+		var startOfDay = new Date(now.getFullYear(), now.getMonth(), now.getDate());
+		var timestamp = startOfDay / 1000;
+		connection.query("SELECT count(id) FROM master.request WHERE master.request.requestDate >= " + timestamp + ";", function (err, result, fields) {
+			if (err) throw err;
+			response.setHeader('Content-Type', 'application/json');
+			response.end(JSON.stringify(result));
+		});
+	} else {
+		response.send('Veuillez vous <a href="/login">connecter</a> pour accèder à ce contenu');
+	}
+})
+
+app.get('/logout', function (req, res) {
+	req.session.destroy(function(err) {
+		res.redirect('/');
+		console.log('Session terminée')
+	})
+})
+
+app.get('/graph', function (req, res) {
+	if (req.session.loggedin) {
+		data =fs.readFileSync('./content/graph.html', 'utf8');
+		res.send(data);
+	} else {
+		res.send('Veuillez vous <a href="/login">connecter</a> pour accèder à ce contenu');
+	}
+	res.end();
+})
+
+app.get('/login', function (req, res) {
+	res.sendFile(path.join(__dirname + '/login.html'));
 })
 
 app.get('/home', function(request, response) {
@@ -80,9 +129,13 @@ app.get('/home', function(request, response) {
 		data =fs.readFileSync('./content/index.html', 'utf8');
 		response.send(data);
 	} else {
-		response.send('Please login to view this page!');
+		response.send('Veuillez vous <a href="/login">connecter</a> pour accèder à ce contenu');
 	}
 	response.end();
 });
 
-app.listen(3000);
+var httpServer = http.createServer(app);
+var httpsServer = https.createServer(credentials, app);
+
+httpServer.listen(8080);
+httpsServer.listen(8443);
